0a51e7bab069013ef16079e16c7d5b6b195cc275
mqxs21 <maxshi239@gmail.com>
2024-07-17 14:57:58 -0700

    gemstone minecraft

--- Assets/Scripts/DetectPlayerOpenDoor.cs
+++ Assets/Scripts/DetectPlayerOpenDoor.cs
@@ -5,9 +5,12 @@ public class DetectPlayerOpenDoor : MonoBehaviour
 {
     public Animator leftDoor;
     public Animator rightDoor;
+    public Collider m_Collider;
     void OnTriggerEnter(Collider obj){
+        
         if (obj.CompareTag("Player"))
         {
+             m_Collider.enabled = false;
             leftDoor.SetBool("playerNear",true);
             rightDoor.SetBool("playerNearRight",true);
         }
--- Assets/collectCoin.cs
+++ Assets/collectCoin.cs
@@ -1,15 +1,49 @@
-
+using System.Linq.Expressions;
+using TreeEditor;
+using Unity.VisualScripting;
+using UnityEditor.Callbacks;
 using UnityEngine;
 
 public class collectCoin : MonoBehaviour
 {
-    void OnCollisionEnter(Collision obj){
+    public float speed;
+    public bool canCollect = false;
+
+    public Rigidbody rb;
+
+    void OnCollisionEnter(Collision obj)
+    {
         if (obj.gameObject.CompareTag("Player"))
         {
             GameObject.Find("coinTracker").GetComponent<coinTrackerScript>().coinAmount++;
-            
+            Instantiate(GameObject.Find("purpleEffects"),GameObject.Find("Joint").transform.position,Quaternion.identity);
             Destroy(gameObject);
-            
+        }
+    }
+
+    void OnTriggerEnter(Collider obj)
+    {
+        Debug.Log("tig");
+        Debug.Log(obj.name);
+        rb = GetComponent<Rigidbody>();
+        if (obj.gameObject.CompareTag("Player"))
+        {
+            canCollect = true;
+        }
+    }
+
+    void Update()
+    {
+        // Ensure the rotation is always -90 degrees on the x-axis
+        transform.rotation = Quaternion.Euler(-90, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+
+        if (canCollect)
+        {
+            rb.isKinematic = true;
+            Debug.Log("comeTowardsPlayer");
+            Vector3 goToPos = GameObject.Find("PlayerCamera").transform.position;
+            goToPos.y = goToPos.y - 1;
+            transform.position = Vector3.Lerp(transform.position, goToPos, Time.deltaTime * speed);
         }
     }
 }
